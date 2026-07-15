de6b2f3ed563d8eb27b83169cd8d517d9bd89501
luka <lukadjuric013@gmail.com>
2026-07-15 22:42:47 +0800

    Add: length-of-last-word - submission-2

---
+++ Data Structures & Algorithms/length-of-last-word/submission-2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.length()-1;
+
+        while(n >= 0 && s[n] == ' ') --n;
+        int cnt = 0;
+        while(n >=0 && s[n--] != ' ') ++cnt;
+
+        return cnt;
+    }
+};

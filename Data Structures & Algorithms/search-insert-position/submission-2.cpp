dbca658508398cfc8d70c92331e9cc263d0d9732
luka <lukadjuric013@gmail.com>
2026-08-04 12:02:50 +0800

    Add: search-insert-position - submission-2

---
+++ Data Structures & Algorithms/search-insert-position/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size()-1;
+        int mid;
+        while(l <= r){
+            mid = r - (r - l)/2;
+            if(target == nums[mid]) return mid;
+            if(nums[mid] < target) l = mid+1;
+            else r = mid-1;
+        }
+
+        return nums[mid] < target ? mid+1 : mid;
+
+
+    }
+};
